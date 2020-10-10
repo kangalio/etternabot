a9bddc735d46aec45734b490634e272e4ba2e2c9
kangalioo <jannik.a.schaper@web.de>
2020-10-10 15:42:19 +0200

    Show mine hit icons in replay graph

--- src/discord_handler/replay_graph.rs
+++ src/discord_handler/replay_graph.rs
@@ -10,6 +10,8 @@ const GOOD_COLOR: RGBColor = RGBColor(0x1A, 0xB2, 0xFF);
 const BAD_COLOR: RGBColor = RGBColor(0xFF, 0x1A, 0xB3);
 const MISS_COLOR: RGBColor = RGBColor(0xCC, 0x29, 0x29);
 
+const ICON_SIZE: usize = 22;
+
 /// Takes a deviation in seconds, positive or negative, and generates the appropriate judgement
 /// color
 fn deviation_to_color(deviation: f32) -> RGBColor {
@@ -23,6 +25,48 @@ fn deviation_to_color(deviation: f32) -> RGBColor {
 	}//.to_rgba().mix(0.5)
 }
 
+struct IconStamper {
+	icon: BitMapElement<'static, (i32, i32), plotters_bitmap::bitmap_pixel::RGBPixel>
+}
+
+impl IconStamper {
+	pub fn new(path: &str, size: u32) -> Result<Self, Box<dyn std::error::Error>> {
+		use image::Pixel as _;
+
+		let image = image::open(path)?;
+
+		// convert RGBA->RGB, making transparent pixels black
+		// plotters crate and image crate have united in their goal to make us do the most awkward
+		// fucking stupid dances to do basic things
+		let mut image = image.into_rgba();
+		for pixel in image.pixels_mut() {
+			let mut new_pixel = image::Rgba([0, 0, 0, 255]);
+			new_pixel.blend(&pixel);
+			*pixel = new_pixel;
+		}
+		let image = image::DynamicImage::ImageRgb8(image::DynamicImage::ImageRgba8(image).into_rgb());
+		
+		// Triangle (aka bilinear) is the fastest resize algorithm that doesn't look garbage
+		let image = image.resize(size, size, image::imageops::FilterType::Triangle);
+
+		let icon = plotters::element::BitMapElement::from(((0, 0), image));
+		Ok(Self { icon })
+	}
+
+	pub fn stamp_onto<DB>(&mut self,
+		canvas: &plotters::drawing::DrawingArea<DB, plotters::coord::Shift>,
+		coord: (i32, i32),
+	) -> Result<(), Box<dyn std::error::Error>>
+	where
+		DB: plotters::prelude::DrawingBackend,
+		for<'a> &'a plotters::element::BitMapElement<'a, (i32, i32)>: plotters::element::PointCollection<'a, (i32, i32)>, // I don't even fucking know
+	{
+		self.icon.move_to(coord);
+		canvas.draw(&self.icon).map_err(|e| e.to_string())?;
+		Ok(())
+	}
+}
+
 pub fn inner(
 	replay: &eo::Replay,
 	output_path: &str
@@ -30,6 +74,7 @@ pub fn inner(
 	let notes = &replay.notes;
 
 	let mut hits: Vec<(f32, f32)> = Vec::new();
+	let mut mine_hit_locations: Vec<f32> = Vec::new();
 	let mut points = 0.0;
 	let mut min_wifescore = f32::INFINITY;
 	let mut max_wifescore = f32::NEG_INFINITY;
@@ -56,12 +101,14 @@ pub fn inner(
 			},
 			eo::NoteType::Mine => {
 				points += etterna::Wife3::MINE_HIT_WEIGHT;
+				mine_hit_locations.push(note.time);
 			},
 			eo::NoteType::HoldTail | eo::NoteType::Fake | eo::NoteType::Keysound => {},
 		}
 	}
 	// println!("final wifescore: {}", hits[hits.len() - 1].1);
 
+	// can't use the one-liner iterator method because float doesn't impl Ord >:(
 	let mut chart_length = 0.0;
 	for note in notes {
 		if note.time as f32 > chart_length {
@@ -75,14 +122,24 @@ pub fn inner(
 	let wifescore_chart_x_range = 0.0f32..chart_length;
 	let wifescore_range = max_wifescore - min_wifescore;
 	let wifescore_chart_y_range = (min_wifescore - wifescore_range / 10.0)..(max_wifescore + wifescore_range / 10.0);
-
+	
 	let acc = wifescore_range < 0.5; // if true, the axis labels are more precise
-
+	
 	let mut wifescore_chart = ChartBuilder::on(&root)
-		.build_cartesian_2d(wifescore_chart_x_range.clone(), wifescore_chart_y_range.clone())?;
-
+	.build_cartesian_2d(wifescore_chart_x_range.clone(), wifescore_chart_y_range.clone())?;
+	
+	// we leave a bit of space on the top for aesthetics, and even more space on the bottom to fit
+	// the mine hit icons
 	let mut dots_chart = ChartBuilder::on(&root)
-		.build_cartesian_2d(0.0f32..chart_length, -0.19..0.19f32)?;
+		.build_cartesian_2d(0.0f32..chart_length, -0.20..0.19f32)?;
+	
+	let mut mine_hit_icon = IconStamper::new("assets/icons/mine-hit.png", 24)?;
+	for mine_hit_location in mine_hit_locations {
+		mine_hit_icon.stamp_onto(&root, (
+			dots_chart.backend_coord(&(mine_hit_location, 0.0)).0 - ICON_SIZE as i32 / 2 + 4,
+			dots_chart.plotting_area().get_y_axis_pixel_range().end - ICON_SIZE as i32,
+		))?;
+	}
 
 	let draw_horizontal_line = |height: f32, color: &RGBColor| {
 		let path = PathElement::new(vec![
