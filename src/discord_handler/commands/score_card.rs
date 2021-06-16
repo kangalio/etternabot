890f4d28e3daa068a535202227c2257991e10c40
kangalioo <jannik.a.schaper@web.de>
2021-06-16 16:18:44 +0200

    Improve InvalidJudge error message

--- src/discord_handler/commands/score_card.rs
+++ src/discord_handler/commands/score_card.rs
@@ -7,7 +7,7 @@ use crate::Error;
 pub struct InvalidJudge;
 impl std::fmt::Display for InvalidJudge {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.write_str("Invalid judge")
+		f.write_str("Please write the judge like `J5` or `J8`")
 	}
 }
 impl std::error::Error for InvalidJudge {}
